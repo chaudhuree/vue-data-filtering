f65614c2df277c1e0d660a78ba767de30abaa121
chaudhuree <chaudhuree00@gmail.com>
2023-11-30 12:13:24 +0600

    Add data fetching from API and display in DataTable component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
-
+import { onMounted,ref } from "vue";
+import DataTable from "./components/DataTable.vue";
+const items = ref([]);
+onMounted(async () => {
+  const response = await fetch("/api/tasks");
+  const tasks = await response.json();
+  console.log('tasks',tasks);
+  
+  items.value = tasks;
+});
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Vue data fitering app</h1>
+  <h1 class="p-8 bg-gray-100 min-h-screen">
+    <DataTable :items="items" />
+  </h1>
 </template>
-
-
---
+++ client/src/components/DataTable.vue
@@ -0,0 +1,41 @@
+<script setup>
+defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="bg-white relative border rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500">
+      <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+        <tr>
+          <th class="px-4 py-3">ID</th>
+          <th class="px-4 py-3">Assigned</th>
+          <th class="px-4 py-3">Status</th>
+          <th class="px-4 py-3">Title</th>
+          <th class="px-4 py-3">Due At</th>
+          <th class="px-4 py-3">
+            <span class="sr-only">Actions</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.id" class="border-b">
+          <td class="px-4 py-3 font-medium text-gray-900">{{ item.id }}</td>
+          <td class="px-4 py-3 font-medium text-gray-900">
+            {{ item.assigned_to }}
+          </td>
+          <td class="px-4 py-3 font-medium text-gray-900">
+            {{ item.status.charAt(0).toUpperCase() + item.status.slice(1) }}
+          </td>
+          <td class="px-4 py-3 font-medium text-gray-900">{{ item.title }}</td>
+          <td class="px-4 py-3 font-medium text-gray-900">{{ item.due_at }}</td>
+          <td class="px-4 py-3 font-medium text-gray-900">Details</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
