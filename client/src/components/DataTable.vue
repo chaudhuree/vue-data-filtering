1645c75cfa15cbafb4954917279874a569e14a8d
chaudhuree <chaudhuree00@gmail.com>
2023-11-30 12:39:08 +0600

    Refactor DataTable component to include search functionality

--- client/src/components/DataTable.vue
+++ client/src/components/DataTable.vue
@@ -1,14 +1,42 @@
 <script setup>
-defineProps({
+import { computed, defineProps, ref } from "vue";
+// import FilterDropdown from "./FilterDropdown.vue";
+// import FilterRadios from "./FilterRadios.vue";
+import SearchForm from "./SearchForm.vue";
+const searchTerm = ref("");
+const props = defineProps({
   items: {
     type: Array,
     required: true,
   },
 });
+const filteredItems = computed(() => {
+  if (searchTerm.value !== "") {
+    return props.items.filter((item) => {
+      return (
+        item.title.toLowerCase().includes(searchTerm.value.toLowerCase()) ||
+        item.assigned_to.toLowerCase().includes(searchTerm.value.toLowerCase())
+      );
+    });
+  }
+  return props.items;
+});
+const handleSearch = (value) => {
+  searchTerm.value = value;
+};
 </script>
 
 <template>
   <div class="bg-white relative border rounded-lg">
+    <div class="flex items-center justify-between">
+      <SearchForm @search="handleSearch" />
+      <div class="flex items-center justify-end text-sm font-semibold">
+        <!-- <FilterRadios />
+        <FilterDropdown /> -->
+      </div>
+    </div>
+  </div>
+  <div>
     <table class="w-full text-sm text-left text-gray-500">
       <thead class="text-xs text-gray-700 uppercase bg-gray-50">
         <tr>
@@ -23,7 +51,7 @@ defineProps({
         </tr>
       </thead>
       <tbody>
-        <tr v-for="item in items" :key="item.id" class="border-b">
+        <tr v-for="item in filteredItems" :key="item.id" class="border-b">
           <td class="px-4 py-3 font-medium text-gray-900">{{ item.id }}</td>
           <td class="px-4 py-3 font-medium text-gray-900">
             {{ item.assigned_to }}
---
+++ client/src/components/SearchForm.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineEmits } from "vue";
+const emit = defineEmits(["search"]);
+const handleSearch = (event) => {
+  emit("search", event.target.value);
+};
+</script>
+<template>
+  <form class="py-3 px-4 flex items-center">
+    <label class="sr-only">Search</label>
+    <input
+      type="text"
+      class="w-full bg-gray-50 border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-offset-blue-500"
+      placeholder="Search"
+      @input="handleSearch"
+    />
+  </form>
+</template>
